d78639a23a08f4ac6f0215a4c33dd5adbc9d521e
ɢᴏᴘᴀʟ ᴋʀɪᴤʜɴᴀ ᴍᴏʜᴀᴘᴀᴛʀᴀ <110560006+xtocodex@users.noreply.github.com>
2023-12-22 15:46:13 +0000

    dataTypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,33 @@
+"use strict"; // trats all JS Code as newer version 
+
+// alert(2 + 2) 
+// we are using nodejs not browser so we get error here but it gives popup answer in browser console
+// but we will know how to use it here later.
+
+// 
+// above is very bad code practise don't write like this although it will run
+// but it is not future proof readablity is poor; should be neat and high..
+
+// ->> rewriting- console.log(3 + 3); console.log("Jarvis"); <<-
+
+console.log(3 + 3) 
+console.log("Google")
+let name = "jarvis"
+let age = 18
+let isLoggedIn = false
+let state = null
+
+//number => 2 to power 53
+// bigint (used to store veryy>>> large value/num used in trading sys,algo etc.)
+// string =>""
+// boolean => true/false
+// null => standalone value means doesn't has any value but not ZERO.
+// undefined => (no value has been assigned)
+// symbol => unique
+// object
+
+console.log(typeof undefined); // It's a type of - undefined
+console.log(typeof null); // It's a type of - object
+// But these question the fundamentals why null is recogn. as object and undefined as undefined ??
+// We will find later on.
+// Revise the MDN Docs for this context.
