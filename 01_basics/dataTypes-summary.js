42d5eb2f6b5131183d289545249e2eceaebeeb9e
ɢᴏᴘᴀʟ ᴋʀɪᴤʜɴᴀ ᴍᴏʜᴀᴘᴀᴛʀᴀ <110560006+xtocodex@users.noreply.github.com>
2023-12-24 06:24:46 +0000

    dataTypes Summary with docs

---
+++ 01_basics/dataTypes-summary.js
@@ -0,0 +1,45 @@
+// #Primitive
+
+// 7 types  : String, Number, Boolean, null, undefined, Symbol, BigInt.
+
+const score = 100
+const scoreValue = 100.3
+
+
+const isLoggedIn = false
+const outsideTemp = null
+let userEmail; // if u dont want to give any value leav it like this it will show undefined also u can set = undefined by yourself
+
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+console.log(id === anotherId);
+
+//const bigNumber = 345500220011003308151515151
+//const bigNumber = 345500220011003308151515151n // if we add n at last it will take as BigInt
+
+
+
+
+// Refernce type (Non primitive)
+
+// Array, Objecs, Functions
+
+//example of array,objects etc:-
+
+const heros = ["shaktiman", "naggraj", "doga"]
+let myObj = { 
+    name: "jarvis",
+    age: 21,
+}
+
+// Declare a function like varibale--
+
+const myFunction = function(){
+    console.log("Hello World");
+}
+
+console.log(typeof myFunction); // take others and check there typeof all const taken above.. then readDoc
+
+// Read typeof more info-  https://262.ecma-international.org/5.1/#sec-11.4.3
